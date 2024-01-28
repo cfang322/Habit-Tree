23c43f45ca82cb6c5c7c2158fbf11ec23923af54
Farhat Sumaiya <Farhattsumaiya@gmail.com>
2024-01-28 18:31:50 -0500

    style the loginform and signup

--- frontend/src/components/Routes/Routes.jsx
+++ frontend/src/components/Routes/Routes.jsx
@@ -5,10 +5,7 @@ export const AuthRoute = ({ component: Component, ...props }) => {
   const loggedIn = useSelector((state) => !!state.session.user);
 
   return !loggedIn ? (
-    <>
-      <Component {...props} />
-      {/* <Navigate to="/" replace={true} /> */}
-    </>
+    <Component {...props} />
   ) : (
     <Navigate to="/feed" replace={true} />
   );
