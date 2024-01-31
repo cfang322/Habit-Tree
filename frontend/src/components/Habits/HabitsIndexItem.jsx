e3c452eb1536cf88cb688613951ed39af943694f
Yaqi Fang <cfang322@gmail.com>
2024-01-31 16:02:22 -0500

    added placeholder for gsap animation

--- frontend/src/components/Habits/HabitsIndexItem.jsx
+++ frontend/src/components/Habits/HabitsIndexItem.jsx
@@ -5,6 +5,7 @@ import * as modalActions from "../../store/reducers/modals";
 import CreateHabit from "./CreateHabit";
 import { useEffect, useState } from "react";
 import './HabitsIndexItem.css';
+import placeholder from '../../assets/placeholder.jpg';
 
 const HabitIndexItem = () => {
   const navigate = useNavigate();
@@ -39,36 +40,39 @@ const HabitIndexItem = () => {
   }
   
   return (
-    <div className="habit">
-      {editMode ? (
-        <CreateHabit
-          editMode={true}
-          habitToEdit={habit}
-          handleCloseModal={handleCloseModal}
-        />
-      ) : (
-        <>
-          <div className="allTheHabits">
-            <h1>{habit.name}</h1>
-            <ul className="habit-detail">Category: <li>{habit.category}</li></ul>
-            <ul className="habit-detail">Habit: <li>{habit.habitType}</li></ul>
-            <ul className="habit-detail">Achieved: <li>{habit.achieved}</li></ul>
-            <ul className="habit-detail">Goal: <li>{habit.goal}</li></ul>
-            <ul className="habit-detail">Goal Period: <li>{habit.goalPeriod}</li></ul>
-            <ul className="habit-detail">Start Date: <li>{habit.startDate ? new Date(habit.startDate).toLocaleDateString() : 'Not Specified'}</li></ul>
-            <ul className="habit-detail">End Date: <li>{habit.endDate ? new Date(habit.endDate).toLocaleDateString() : 'Not Specified'}</li></ul>
-            <ul className="habit-detail">Completed: <li>{habit.completed}</li></ul>
-          </div>
-          <div className="buttonsDiv">
-            <div className="editDiv">
-              <button onClick={handleEdit}>Edit</button>
+    <div className="habit-container">
+      <img src={placeholder} alt="gsap-image" className="habit-image" height={500} width={500}/>
+      <div className="habit">
+        {editMode ? (
+          <CreateHabit
+            editMode={true}
+            habitToEdit={habit}
+            handleCloseModal={handleCloseModal}
+          />
+        ) : (
+          <>
+            <div className="allTheHabits">
+              <h1>{habit.name}</h1>
+              <ul className="habit-detail">Category: <li>{habit.category}</li></ul>
+              <ul className="habit-detail">Habit: <li>{habit.habitType}</li></ul>
+              <ul className="habit-detail">Achieved: <li>{habit.achieved}</li></ul>
+              <ul className="habit-detail">Goal: <li>{habit.goal}</li></ul>
+              <ul className="habit-detail">Goal Period: <li>{habit.goalPeriod}</li></ul>
+              <ul className="habit-detail">Start Date: <li>{habit.startDate ? new Date(habit.startDate).toLocaleDateString() : 'Not Specified'}</li></ul>
+              <ul className="habit-detail">End Date: <li>{habit.endDate ? new Date(habit.endDate).toLocaleDateString() : 'Not Specified'}</li></ul>
+              <ul className="habit-detail">Completed: <li>{habit.completed}</li></ul>
             </div>
-            <div className="deleteDiv">
-              <button onClick={handleDelete}>Delete</button>
+            <div className="buttonsDiv">
+              <div className="editDiv">
+                <button onClick={handleEdit}>Edit</button>
+              </div>
+              <div className="deleteDiv">
+                <button onClick={handleDelete}>Delete</button>
+              </div>
             </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
