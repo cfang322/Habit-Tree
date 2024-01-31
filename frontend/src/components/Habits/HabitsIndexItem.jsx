8a702654f78343e19651ff4be5d00b44ee35235e
Yaqi Fang <cfang322@gmail.com>
2024-01-31 11:43:45 -0500

    Styled fonts and sizing for the categories in habit show page and added navbar

--- frontend/src/components/Habits/HabitsIndexItem.jsx
+++ frontend/src/components/Habits/HabitsIndexItem.jsx
@@ -1,14 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useParams } from "react-router";
-import {
-  deleteHabit,
-  fetchHabits,
-  selectAllHabitsArray,
-  // updateHabit,
-} from "../../store/reducers/habits";
+import { deleteHabit, fetchHabits, selectAllHabitsArray } from "../../store/reducers/habits";
 import * as modalActions from "../../store/reducers/modals";
 import CreateHabit from "./CreateHabit";
 import { useEffect, useState } from "react";
+import './HabitsIndexItem.css';
 
 const HabitIndexItem = () => {
   const navigate = useNavigate();
@@ -16,58 +12,62 @@ const HabitIndexItem = () => {
   const { habitId } = useParams();
   const habits = useSelector(selectAllHabitsArray);
   const habit = habits.find((habit) => habit._id === habitId);
-  // const modalType = useSelector((state) => state.modals.type === "SHOW_HABITS");
-
+  
   const [editMode, setEditMode] = useState(false);
-
+  
   useEffect(() => {
     dispatch(fetchHabits());
   }, [dispatch, habitId]);
-
+  
   const handleDelete = () => {
     dispatch(deleteHabit(habitId));
     navigate("/feed");
   };
-
+  
   const handleEdit = () => {
     setEditMode(true);
     dispatch(modalActions.showModal("SHOW_HABITS"));
   };
-
+  
   const handleCloseModal = () => {
     setEditMode(false);
     dispatch(modalActions.hideModal());
   };
-
+  
   if (!habit) {
     return <div>Habit not found</div>;
   }
-
+  
   return (
     <div className="habit">
-      <div>
-        <button onClick={handleEdit}>Edit</button>
-        <button onClick={handleDelete}>Delete</button>
-      </div>
       {editMode ? (
         <CreateHabit
           editMode={true}
           habitToEdit={habit}
           handleCloseModal={handleCloseModal}
-          // handleUpdate={handleUpdate}
         />
       ) : (
-        <div className="allTheHabits">
-          <h2>{habit.name}</h2>
-          <p>{habit.category}</p>
-          <p>{habit.habitType}</p>
-          <p>{habit.achieved}</p>
-          <p>{habit.goal}</p>
-          <p>{habit.goalPeriod}</p>
-          <p>{habit.startDate}</p>
-          <p>{habit.endDate}</p>
-          <p>{habit.completed}</p>
-        </div>
+        <>
+          <div className="allTheHabits">
+            <h1>{habit.name}</h1>
+            <p>Category: {habit.category}</p>
+            <p>Habit: {habit.habitType}</p>
+            <p>Achieved: {habit.achieved}</p>
+            <p>Goal: {habit.goal}</p>
+            <p>Goal Period: {habit.goalPeriod}</p>
+            <p>Start Date: {habit.startDate}</p>
+            <p>End Date: {habit.endDate}</p>
+            <p>Completed: {habit.completed}</p>
+          </div>
+          <div className="buttonsDiv">
+            <div className="editDiv">
+              <button onClick={handleEdit}>Edit</button>
+            </div>
+            <div className="deleteDiv">
+              <button onClick={handleDelete}>Delete</button>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
