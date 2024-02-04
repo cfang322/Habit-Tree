1a284d7040a2c02ce21e6efddbcbead9c274d6b4
Farhat Sumaiya <Farhattsumaiya@gmail.com>
2024-02-04 15:14:57 -0500

    add to about us

--- frontend/src/components/AboutUs/AboutUs.jsx
+++ frontend/src/components/AboutUs/AboutUs.jsx
@@ -1,54 +1,68 @@
-import './AboutUs.css';
-import christinaProfile from '../../assets/christinaProfile.jpeg';
-import farhatProfile from '../../assets/farhatProfile.jpeg';
-
+import "./AboutUs.css";
+import christinaProfile from "../../assets/christinaProfile.jpeg";
+import farhatProfile from "../../assets/farhatProfile.jpeg";
 
 function AboutUs() {
-    const profiles = [
-        {
-            name: "Farhat Sumaiya",
-            role: "Frontend",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            image: farhatProfile,
-            github: "",
-            linkedin: ""
-        },
-        {
-            name: "Yaqi Fang",
-            role: "Lead",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            image: christinaProfile,
-            github: "https://github.com/cfang322",
-            linkedin: "https://www.linkedin.com/in/yaqi-fang-125807250/"
-        },
-        {
-            name: "Dominic Cashiola",
-            role: "Backend",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-            image: "person3.jpg",
-            github: "",
-            linkedin: ""
-        }
-    ];
-    
-    return (
-        <div className="aboutUsWrapper">
-            <div className="aboutUsContainer">
-                {profiles.map((profile, index) => (
-                    <div key={index} className="personCard">
-                        <img src={profile.image} alt={`profile-img-${index}`} />
-                        <h2>{profile.name}</h2>
-                        <p>{profile.role}</p>
-                        <p>{profile.description}</p>
-                        <div className="socialLinks">
-                            <a className='gitLink' href={profile.github} target="_blank" rel="noopener noreferrer">GitHub</a>
-                            <a className='linkedinLink' href={profile.linkedin} target="_blank" rel="noopener noreferrer">LinkedIn</a>
-                        </div>
-                    </div>
-                ))}
+  const profiles = [
+    {
+      name: "Farhat Sumaiya",
+      role: "Frontend",
+      description:
+        "As a beginner full stack developer, I'm dedicated to creating user-friendly websites and applications while constantly learning and adapting to new technologies",
+      image: farhatProfile,
+      github: "https://github.com/Farhatt18",
+      linkedin: "https://www.linkedin.com/in/farhat-sumaiya-4704b11a3/",
+    },
+    {
+      name: "Yaqi Fang",
+      role: "Lead",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      image: christinaProfile,
+      github: "https://github.com/cfang322",
+      linkedin: "https://www.linkedin.com/in/yaqi-fang-125807250/",
+    },
+    {
+      name: "Dominic Cashiola",
+      role: "Backend",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      image: "person3.jpg",
+      github: "",
+      linkedin: "",
+    },
+  ];
+
+  return (
+    <div className="aboutUsWrapper">
+      <div className="aboutUsContainer">
+        {profiles.map((profile, index) => (
+          <div key={index} className="personCard">
+            <img src={profile.image} alt={`profile-img-${index}`} />
+            <h2>{profile.name}</h2>
+            <p>{profile.role}</p>
+            <p>{profile.description}</p>
+            <div className="socialLinks">
+              <a
+                className="gitLink"
+                href={profile.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+              <a
+                className="linkedinLink"
+                href={profile.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
             </div>
-        </div>
-    );
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
