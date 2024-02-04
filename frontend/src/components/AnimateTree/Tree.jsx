0a8e0b9aaa3c416891ff57e4fe7e5a5036867a14
Dominic <domcashiola@gmail.com>
2024-02-04 15:28:31 -0500

    working on styling about us

--- frontend/src/components/AnimateTree/Tree.jsx
+++ frontend/src/components/AnimateTree/Tree.jsx
@@ -1,18 +1,21 @@
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
-import treePng from '../../assets//treePng/tree1.png'
+import treePng from '../../assets/tree.png';
 
 const Tree = ({ progress }) => {
   const treeRef = useRef(null);
+  const maxHeight = 100;
+  const maxWidth = 100;
 
   useEffect(() => {
+    const limitedProgressHeight = Math.min(progress, maxHeight);
+    const limitedProgressWidth = Math.min(progress, maxWidth);
+
     const treeAnimation = gsap.timeline();
 
-    // Your tree drawing animation logic here
-    // For simplicity, let's just scale the tree based on progress
-    treeAnimation.to(treeRef.current, { scaleX: progress / 100, scaleY: progress / 100, duration: 1 });
 
-    // Cleanup the animation on component unmount
+    treeAnimation.to(treeRef.current, { scaleX: limitedProgressWidth / 100, scaleY: limitedProgressHeight / 100, duration: 0 });
+
     return () => treeAnimation.kill();
   }, [progress]);
 
